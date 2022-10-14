e8ee8059fa0667e368cde65c66e2b87969e5fce9
RetoRuto9900K <41989402+tutinoko2048@users.noreply.github.com>
2022-10-14 19:50:55 +0900

    async, typeId, entityCheck

--- scripts/ac.js
+++ scripts/ac.js
@@ -4,24 +4,24 @@ Made by RetoRuto9900K @tutinoko_kusaa
 */
  
 import { world, ItemStack, MinecraftItemTypes, MinecraftBlockTypes, MinecraftEnchantmentTypes, Location } from '@minecraft/server';
-import { sendCmd, detected } from './index.js';
+import { detected } from './util/util';
 import config from './config.js';
-import './util/timer.js'; // timer.js v1.1 by lapis256 | https://github.com/Lapis256/timer.js/blob/main/LICENSE
-
+import './util/timer'; // timer.js v1.1 by lapis256 | https://github.com/Lapis256/timer.js/blob/main/LICENSE
 let loaded = false;
+let startTime = Date.now();
 
 const airBlock = MinecraftBlockTypes.air.createDefaultBlockPermutation(); // air block permutation
 const airItem = new ItemStack(MinecraftItemTypes.stick, 0, 0);
 
 world.events.tick.subscribe(ev => {
   if (!loaded) {
-    try {
-      world.getDimension('overworld').runCommand('testfor @a');
-      loaded = true;
-      world.say('[TN-AntiCheat] ac.js >> loaded');
-    } catch {}
+    world.getDimension('overworld').runCommandAsync('testfor @a')
+      .then(() => {
+        loaded = true;
+        world.say(`[TN-AntiCheat] ac.js >> loaded (${Date.now() - startTime}ms)`);
+      })
+      .catch(() => {});
   } else {
-  
     for (const player of world.getPlayers()) {
        
        if (config.crasher.state) { // Crasher detection by Scythe-AntiCheat
@@ -47,7 +47,7 @@ world.events.tick.subscribe(ev => {
             let badEnchant = enchantCheck(item);
             if (badEnchant) {
               container.setItem(player.selectedSlot, badEnchant.itemStack);
-              detected(`§l§c${player.name}§r >> オーバーエンチャントを検知しました (ID: ${badEnchant.itemStack.id}, Enchant: ${badEnchant.id}, Level: ${badEnchant.level})`);
+              detected(`§l§c${player.name}§r >> オーバーエンチャントを検知しました (ID: ${badEnchant.itemStack.typeId}, Enchant: ${badEnchant.id}, Level: ${badEnchant.level})`);
             }
           } 
           
@@ -57,17 +57,17 @@ world.events.tick.subscribe(ev => {
           let item = container.getItem(i);
           if (!item) continue;
           
-          if (config.itemCheck.detect.includes(item.id) || (config.itemCheck.spawnEgg && item.id.endsWith('spawn_egg')) ) {
+          if (config.itemCheck.detect.includes(item.typeId) || (config.itemCheck.spawnEgg && item.typeId.endsWith('spawn_egg')) ) {
             container.setItem(i, airItem);
             let name = item.nameTag ? `${item.nameTag.replace(/\n/g, '\\n').slice(0,20)}${item.nameTag.length>20 ? '§r...' : '§r'}` : null
-            player.kick(`禁止アイテムの所持を検知しました  (ID: §c${item.id}:${item.data}${name ? `§r, Name: ${name}§r` : ''})`);
+            player.kick(`禁止アイテムの所持を検知しました  (ID: §c${item.typeId}:${item.data}§r${name ? `, Name: ${name}§r` : ''})`);
           }
           
           if (config.enchantCheck.state && config.enchantCheck.mode == 'inventory') {
             let badEnchant = enchantCheck(item);
             if (badEnchant) {
               container.setItem(i, badEnchant.itemStack);
-              detected(`§l§c${player.name}§r >> オーバーエンチャントを検知しました (ID: ${badEnchant.itemStack.id}, Enchant: ${badEnchant.id}, Level: ${badEnchant.level})`);
+              detected(`§l§c${player.name}§r >> オーバーエンチャントを検知しました (ID: ${badEnchant.itemStack.typeId}, Enchant: ${badEnchant.id}, Level: ${badEnchant.level})`);
             }
           }
         }
@@ -92,42 +92,27 @@ world.events.tick.subscribe(ev => {
 world.events.beforeItemUseOn.subscribe(ev => { // 禁止ブロックを設置したら引っかかるよ
   let {source, item} = ev;
   if (source.hasTag(config.tag.op)) return;
-  if (config.placeCheck.state && config.placeCheck.detect.includes(item.id)) {
+  if (config.placeCheck.state && config.placeCheck.detect.includes(item.typeId)) {
     ev.cancel = true;
-    source.kick(`禁止アイテムの使用を検知しました (ID: §c${item.id}:${item.data}§r)`);
+    source.kick(`禁止アイテムの使用を検知しました (ID: §c${item.typeId}:${item.data}§r)`);
   }
 });
 
+const despawnable = [ 'minecraft:command_block_minecart', 'minecraft:npc'];
 world.events.entityCreate.subscribe(ev => { // 禁止エンティティが出されたら引っかかるよ
   let {entity} = ev;
-  let id = entity.id;
-  if (config.entityCheck.state && config.entityCheck.detect.includes(entity.id)) {
-    if (id == 'minecraft:command_block_minecart') {
-      entity.kill();
-      return detected(`禁止エンティティを検知したためkillしました (ID: §c${id}§r)`);
-    }
-    if (id === 'minecraft:npc') {
-      try {
-        entity.kill();
-        entity.triggerEvent('tn:despawn');
-      } catch {}
-      return detected(`禁止エンティティを検知したためkillしました (ID: §c${id}§r)`);
-    }
-    setTimeout(() => {
-      try {
-        entity.kill();
-        detected(`禁止エンティティを検知したためkillしました (ID: §c${id}§r)`);
-      } catch {}
-    },1);
-  }
-  
-  if (config.itemCheck.drop && entity.id == 'minecraft:item') {
+  if (config.entityCheck.state && config.entityCheck.detect.includes(entity.typeId)) {
+    const id = entity.typeId;
+    entity.kill();
+    detected(`禁止エンティティを検知したためkillしました (ID: §c${id}§r)`);
+    if (despawnable.includes(id)) 
+      try { entity.triggerEvent('tn:despawn') } catch {}
+    
+  } else if (config.itemCheck.drop && entity.typeId == 'minecraft:item') {
     let item = entity.getComponent('item').itemStack;
-    if (config.itemCheck.detect.includes(item.id)) {
-      setTimeout(() => {
-        entity.kill();
-        detected(`禁止アイテムを検知したためkillしました (ID: §c${item.id}§r) `);
-      }, 1);
+    if (config.itemCheck.detect.includes(item.typeId)) {
+      entity.kill();
+      detected(`禁止アイテムを検知したためkillしました (ID: §c${item.typeId}§r) `);
     }
   }
 });
@@ -151,21 +136,21 @@ world.events.beforeChat.subscribe(ev => {
 world.events.blockPlace.subscribe(ev => { // チェスト設置時に中身をスキャン
   if (!config.containerCheck.state) return;
   let {block,player} = ev;
-  if (!config.containerCheck.detect.includes(block.id)) return;
+  if (!config.containerCheck.detect.includes(block.typeId)) return;
   let container = block.getComponent('inventory')?.container;
   if (!container) return;
   let out = [];
   for (let i=0; i<container.size; i++) {
     let item = container.getItem(i);
-    if (item && config.itemCheck.detect.includes(item.id)) {
+    if (item && config.itemCheck.detect.includes(item.typeId)) {
       container.setItem(i, airItem);
       let name = item.nameTag ? `${item.nameTag.replace(/\n/g, '\\n').slice(0,20)} ${item.nameTag.length>20 ? '§r...' : '§r'}` : null;
-      out.push(`ID: §c${item.id}:${item.data}${name ? `§r, Name: §c${name}§r` : ''}`);
+      out.push(`ID: §c${item.typeId}:${item.data}${name ? `§r, Name: §c${name}§r` : ''}`);
     }
   }
   if (out.length > 0) {
     let {x,y,z} = block;
-    detected(`§l§c${player.name}§r >> 禁止アイテムの入った ${block.id} の設置を検知しました [${x} ${y} ${z}]\n${out.slice(-5).join('\n')}\n${out.length>5 ? `more ${out.length-5} items...` : ''}`);
+    detected(`§l§c${player.name}§r >> 禁止アイテムの入った ${block.typeId} の設置を検知しました [${x} ${y} ${z}]\n${out.slice(-5).join('\n')}\n${out.length>5 ? `more ${out.length-5} items...` : ''}`);
   }
 });
 
