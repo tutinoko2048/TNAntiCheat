c33367bc34d69ddc65d1fc062534d9502b14bf56
RetoRuto9900K <41989402+tutinoko2048@users.noreply.github.com>
2022-10-14 19:39:04 +0900

    runCommand→Async, move function to util

--- scripts/index.js
+++ scripts/index.js
@@ -8,39 +8,14 @@ system.events.beforeWatchdogTerminate.subscribe(ev => {
   ev.cancel = true;
 });
 
-Player.prototype.kick = function (reason = 'No reason') {
+Player.prototype.kick = async function (reason = 'No reason') {
   if (this.hasTag(config.tag.op)) return;
   try {
-    this.runCommand(`kick "${this.name}" §f§lKicked by TN-AntiCheat\n§cReason: §r${reason}`); // 普通はこっち
+    await this.runCommandAsync(`kick "${this.name}" §f§lKicked by TN-AntiCheat\n§cReason: §r${reason}`); // 普通はこっち
     detected(`Kicked §l§c${this.name}§r >> ${reason}`);
   } catch {
     // ビヘイビア側でinstant_despawnすれば§"な名前の人でも蹴れます。再参加可能なので注意
     this.triggerEvent('tn:kick'); // 変な名前で蹴れない時はこっち
     detected(`Kicked §l§c${this.name}§r (addon) >> ${reason}`);
   }
-}
-
-export function detected(message) {
-  if (config.others.sendws) { // say
-    sendCmd(`say "[TN-AntiCheat] ${message}"`);
-  } else { // tellraw
-    world.say(`[TN-AntiCheat] ${message}`);
-  }
-}
-
-export function sendCmd(command) {
-  try {
-    return world.getDimension('overworld').runCommand(command);
-  } catch(e) {
-    //console.error('cmdError:',e);
-    return {err: e.message}
-  }
-}
-
-export function sendMsg(msg, target = '@a') {
-  if (!target.match(/@s|@p|@a|@r|@e/)) target = `"${target}"`;
-  let rawtext = JSON.stringify({
-    rawtext: [{ text: String(msg) }]
-  });
-  sendCmd(`tellraw ${target} ${rawtext}`);
 }
