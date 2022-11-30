731bb9fba8a03e872999916ab02eea65ef0a1e6c
RetoRuto9900K <41989402+tutinoko2048@users.noreply.github.com>
2022-11-30 15:01:17 +0900

    Create index.js

---
+++ .github/workflows/manifest/index.js
@@ -0,0 +1,17 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const fs = require('fs');
+
+try {
+  const json = fs.readFileSync('manifest.json').toString();
+  const manifest = JSON.parse(json);
+  const name = manifest.header.name.split(' ')[0];
+  const version = typeof manifest.header.version == 'number'
+    ? manifest.header.version.join('.')
+    : manifest.header.version;
+  const fileName = `${name}_v${version}.mcpack`
+  core.setOutput("fileName", fileName);
+  
+} catch (error) {
+  core.setFailed(error.message + error.stack);
+}
