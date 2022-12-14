88160b7515553388d64a2b63c46ffd2e9de7183e
RetoRuto9900K <41989402+tutinoko2048@users.noreply.github.com>
2022-12-14 12:22:26 +0000

    fix: instaBreak blockId

--- scripts/modules/nuker.js
+++ scripts/modules/nuker.js
@@ -33,13 +33,14 @@ export function nukerBreak(ev) {
 export function instaBreak(ev) {
   const { block, player, brokenBlockPermutation } = ev;
   if (!config.instaBreak.state || Util.isCreative(player) || Util.isOP(player)) return;
+  const blockId = brokenBlockPermutation.type.id;
   
-  if (config.instaBreak.detect.includes(block.typeId)) {
+  if (config.instaBreak.detect.includes(blockId)) {
     system.run(() => {
       killDroppedItem(block.location, block.dimension);
       if (config.instaBreak.place) block.setPermutation(brokenBlockPermutation);
     }); // 1tick delay
-    Util.flag(player, 'InstaBreak', config.instaBreak.punishment, `InstaBreakの使用を検知しました (§c${block.typeId}§r) §7[${block.x}, ${block.y}, ${block.z}]`);
+    Util.flag(player, 'InstaBreak', config.instaBreak.punishment, `InstaBreakの使用を検知しました (§c${blockId}§r) §7[${block.x}, ${block.y}, ${block.z}]`);
     return true;
   }
 }
