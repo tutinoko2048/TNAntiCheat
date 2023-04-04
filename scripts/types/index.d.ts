75e9ba162e79e320b2fa95450f1417df648509cd
RetoRuto9900K <41989402+tutinoko2048@users.noreply.github.com>
2023-04-04 05:04:31 +0000

    declare types

---
+++ scripts/types/index.d.ts
@@ -0,0 +1,22 @@
+import { ItemStopUseOnEventSignal_deprecated } from "@minecraft/server";
+
+declare module '@minecraft/server' {
+  interface World {
+    arrowSpawnCount?: number;
+    cmdSpawnCount?: number;
+    entityCheck?: any;
+  }
+
+  interface Entity {
+    lastHitAt?: number;
+    threwTridentAt?: number;
+    lastLocation?: import('@minecraft/server').Vector3;
+    lastDimensionId?: string;
+    isMoved?: boolean;
+    breakCount?: number;
+    cps?: number[];
+    autoClickerFlag?: string;
+    reachAFlag?: string;
+    reachBFlag?: string;
+  }
+}
