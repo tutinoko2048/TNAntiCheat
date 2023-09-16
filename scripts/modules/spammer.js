0067fb12555ef4a7a596c5e0a51340c669f09385
Retoruto9900K <tutinoko2048@gmail.com>
2023-09-16 04:28:49 +0000

    feat(Spammer/C): update lastMsgSentAt for too-fast spam

--- scripts/modules/spammer.js
+++ scripts/modules/spammer.js
@@ -30,6 +30,7 @@ export function spammerC(ev) {
   if (sender.lastMsgSentAt && interval < config.spammerC.minInterval) {
     const wait = (config.spammerC.minInterval - interval) / 1000;
     Util.notify(`チャットの送信間隔が速すぎます。 ${wait.toFixed(1)}秒待ってください`, sender);
+    if (interval < 150) sender.lastMsgSentAt = Date.now(); // 特に速すぎる時は完全にブロックする
     return ev.cancel = true; // flag -> return true
   }
   sender.lastMsgSentAt = Date.now();
