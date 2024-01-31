ea96fc4908176f1a11c6c087ae25ed8a848ca93c
RetoRuto9900K <41989402+tutinoko2048@users.noreply.github.com>
2024-01-31 15:21:02 +0900

    fix: update default config

--- scripts/config.js
+++ scripts/config.js
@@ -270,8 +270,8 @@ export default /** @type {import('./types').IConfig} */ ({
   autoClicker: { // 連打ツールの使用を検知
     state: true,
     punishment: "notify",
-    maxCPS: 23,
-    flagCount: 30 // この回数以上検知されるとFlag (-1で無制限)
+    maxCPS: 25,
+    flagCount: 20 // この回数以上検知されるとFlag (-1で無制限)
   },
   creative: { // クリエイティブになったら検知
     state: true,
