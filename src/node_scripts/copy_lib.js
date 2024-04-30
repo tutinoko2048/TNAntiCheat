7c5bb3e8901d2d033ed3d5de338a4498446b9a6c
RetoRuto9900K <tutinoko2048@gmail.com>
2024-04-30 18:05:23 +0900

    rename util->utils

--- src/node_scripts/copy_lib.js
+++ src/node_scripts/copy_lib.js
@@ -2,4 +2,4 @@ const fs = require('fs-extra');
 const path = require('node:path');
 const node_modules = path.join(__dirname, '../../node_modules');
 
-fs.copySync(path.join(node_modules, '@minecraft/vanilla-data/lib'), path.join(__dirname, '../util/vanilla-data'));
+fs.copySync(path.join(node_modules, '@minecraft/vanilla-data/lib'), path.join(__dirname, '../utils/vanilla-data'));
